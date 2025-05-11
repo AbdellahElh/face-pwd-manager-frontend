de2c0a980f90453ce761978778daf488dd805f78
Abdellah El Halimi <abdellah.elhalimimerroun@student.hogent.be>
2025-05-11 22:50:00 +0200

    add credential renamed

--- src/components/PasswordManager.tsx
+++ src/components/PasswordManager.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import useSWR, { mutate } from "swr";
 import { fetchAll, post, remove } from "../data/apiClient";
 import { authService } from "../services/authService";
-import AddPwd from "./AddPwd";
+import AddPwd from "./AddCredential";
 import { CredentialEntry } from "./Credential";
 import CredentialList from "./CredentialList";
 import { ChevronLeftIcon, PlusIcon } from "./icons/Icons";
